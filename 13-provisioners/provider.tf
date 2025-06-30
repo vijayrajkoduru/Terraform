9712eb83d0882ced7085af6985650f4de38279e9
Vijaykumar-Koduru <koduru.vijaykumar.7@gmail.com>
2025-06-30 20:32:58 +0530

    created

---
+++ 13-provisioners/provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.84.0"
+    }
+  }
+
+  
+  backend "s3" {
+    bucket         = "expense-project-1"
+    key            = "VPN-home"
+    region         = "us-east-1"
+    encrypt = true
+    use_lockfile = true
+  }
+}
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
